16a5f6a92ab13d17acbfd4e6e8b5e1a449409e7e
Renato Barros <rennatobarros@gmail.com>
2021-04-19 01:49:42 -0300

    Menu alterado para receber opções vindo do Enum

--- src/bti/ufrn/imd/Programa.java
+++ src/bti/ufrn/imd/Programa.java
@@ -22,34 +22,47 @@ public class Programa {
 		System.out.println(registro.getPix().getLoja().getVendedor("111.222.555-45").getSaldo());
 		
 		//
-		System.out.println("\n0- Sair; \n1 ~ 9- Acessar escolhas:");
+		System.out.println("\n0 ou 5- Sair; \n1 ~ 4- Acessar menu:\n");
+		
+		
 		Scanner escolha = new Scanner(System.in);	
 		String opcao = escolha.nextLine();
 		while(!opcao.equals("0")) {
-			System.out.println("\n1- Imprimir dados cliente;\n2- Imprimir dados vendedor;"
-					+ "\n3- Registrar produto;\n4- Comprar/Vender;\n5 ~ 0- Sair;");
+			String cpf;
+			String cnpj;
+			
+			System.out.println(	"1- " + MenuEnum.IMPRIMIR_DADOS_CLIENTE.getMessage() + "\n" + 
+								"2- " + MenuEnum.IMPRIMIR_DADOS_VENDEDOR.getMessage() + "\n" +
+								"3- " + MenuEnum.REGISTRAR_PRODUTO.getMessage() + "\n" + 
+								"4- " + MenuEnum.COMPRAR_VENDER_PRODUTO.getMessage() + "\n" + 
+								"5 ou 0- " + MenuEnum.FECHAR_PROGRAMA.getMessage());
+
 			String menu = escolha.nextLine();
-			if(menu.equals("1")) {
-				System.out.println("\nDigitar CPF do cliente: ");
-				String cpf = escolha.nextLine();
-				registro.getComprador(cpf);
-			}else if(menu.equals("2")) {
-				System.out.println("\nDigitar CNPJ do Vendedor: ");
-				String cnpj = escolha.nextLine();
-				registro.getVendedor(cnpj);
-			}else if(menu.equals("3")) {
-				System.out.println("\nDigitar CNPJ do Vendedor: ");
-				String cnpj = escolha.nextLine();
-				registro.adicionaProduto(cnpj);
-			}else if(menu.equals("4")) {
-				System.out.println("\nDigitar CPF do cliente: ");
-				String cpf = escolha.nextLine();
-				System.out.println("\nDigitar CNPJ do Vendedor: ");
-				String cnpj = escolha.nextLine();
-				registro.operacao(cpf, cnpj);
-			}else {
-				System.out.println("Saindo...");
-				break;
+			
+			switch(menu) {
+				case "1":
+					System.out.println("\nDigitar CPF do cliente: ");
+					cpf = escolha.nextLine();
+					registro.getComprador(cpf);
+					break;
+				case "2": 
+					System.out.println("\nDigitar CNPJ do Vendedor: ");
+					cnpj = escolha.nextLine();
+					break;
+				case "3": 
+					System.out.println("\nDigitar CNPJ do Vendedor: ");
+					cnpj = escolha.nextLine();
+					registro.adicionaProduto(cnpj);
+					break;
+				case "4":
+					System.out.println("\nDigitar CPF do cliente: ");
+					cpf = escolha.nextLine();
+					System.out.println("\nDigitar CNPJ do Vendedor: ");
+					cnpj = escolha.nextLine();
+					registro.operacao(cpf, cnpj);
+					break;
+				case "5":
+					System.exit(0);
 			}
 		}
 	}
--- src/bti/ufrn/imd/assets/MenuEnum.java
+++ src/bti/ufrn/imd/assets/MenuEnum.java
@@ -1,10 +1,11 @@
 package bti.ufrn.imd.assets;
 
 public enum MenuEnum {
-	DIGITAR_CPF_CLIENTE(1, "Imprimir dados do Cliente"),
-	DIGITAR_CNPJ_VENDEDOR(2, "Digitar CNPJ do Vendedor"),
-	REGISTRAR_PRODUTO(3, "Registrar Produto"),
-	COMPRAR_VENDER(4, "Comprar/Vender");
+	IMPRIMIR_DADOS_CLIENTE(1, "Imprimir dados do Cliente;"),
+	IMPRIMIR_DADOS_VENDEDOR(2, "Digitar CNPJ do Vendedor;"),
+	REGISTRAR_PRODUTO(3, "Registrar Produto;"),
+	COMPRAR_VENDER_PRODUTO(4, "Comprar/Vender;"),
+	FECHAR_PROGRAMA(5, "Digite 5 ou 0 para sair...");
 	
 	private final int value;
 	private final String message;
